68ac38a97bd8470794126f41889b93757a75751c
Magic <mangat.marianna@gmail.com>
2018-06-14 21:56:17 -0700

    Upload Homework

---
+++ 6Week-Python-API/config.py
@@ -0,0 +1,1 @@
+openweatherkey = 'e444fda544927f943f8f58445259f656'
---
+++ 7Week-Social-Analytics/config.py
@@ -0,0 +1,9 @@
+# consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
+# consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
+# access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
+# access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+
+consumer_key = "kTUoKiwvQfDelSdpWCKkSvxvi"
+consumer_secret = "VcK3CluKQWcyXqRZuAgN1XJM4buFGpbj51a9HftaQ7KqjUWQkK"
+access_token = "906829410-U3WPeL04uHKP98l1Qr33Yhv6FiZjZlPoX8qdt7E1"
+access_token_secret = "9wRmlTtsVlQiAhiwIO7yAN2tOQD0SmgbOHx8tf5nM6afG"
